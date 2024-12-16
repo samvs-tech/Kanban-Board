42fd7b9a42746a15fc9f2fb596f297915fa384f0
samvs-tech <mgkelly18@gmail.com>
2024-12-15 21:15:38 -0700

    adjusted models index.ts

--- Develop/server/src/models/index.ts
+++ Develop/server/src/models/index.ts
@@ -1,24 +1,45 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import { Sequelize } from 'sequelize';
-import { UserFactory } from './user.js';
-import { TicketFactory } from './ticket.js';
+import { Sequelize } from "sequelize";
+import { UserFactory } from "./user.js";
+import { TicketFactory } from "./ticket.js";
 
 const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'postgres',
+  ? new Sequelize(process.env.DB_URL, {
+      dialect: "postgres",
       dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
         decimalNumbers: true,
       },
-    });
+    })
+  : new Sequelize(
+      process.env.DB_NAME || "",
+      process.env.DB_USER || "",
+      process.env.DB_PASSWORD || "",
+      {
+        host: process.env.DB_HOST || "localhost",
+        dialect: "postgres",
+        dialectOptions: {
+          ssl:
+            process.env.DB_HOST !== "localhost"
+              ? {
+                  require: true,
+                  rejectUnauthorized: false,
+                }
+              : undefined,
+          decimalNumbers: true,
+        },
+      }
+    );
 
 const User = UserFactory(sequelize);
 const Ticket = TicketFactory(sequelize);
 
-User.hasMany(Ticket, { foreignKey: 'assignedUserId' });
-Ticket.belongsTo(User, { foreignKey: 'assignedUserId', as: 'assignedUser'});
+User.hasMany(Ticket, { foreignKey: "assignedUserId" });
+Ticket.belongsTo(User, { foreignKey: "assignedUserId", as: "assignedUser" });
 
 export { sequelize, User, Ticket };
